07270a51c1a1fc665d3cbf6a4d1be0a8bbc550b4
OmarD <omar.daghestani@colorado.edu>
2022-06-25 12:41:42 -0600

    skip function

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,11 +11,16 @@ var enemyAttack = 12;
 
 console.log(enemyName, enemyAttack, enemyHealth);
 
+var playerMoney = 10;
 
 var fight = function() {
   // Alert players that they are starting the round
   window.alert("Welcome to Robot Gladiators!");
 
+  var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle Enter 'FIGHT' or 'SKIP' to choose.");
+
+  // if player choses to fight, then fight
+  if (promptFight === "fight" || promptFight === "FIGHT") {
   //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
 
   enemyHealth = enemyHealth - playerAttack;
@@ -57,7 +62,27 @@ var fight = function() {
     console.log("Your player is still alive!");
   }
 
+  // if player choses to skip
+} else if (promptFight === "skip" || promptFight === "SKIP") {
+  // confirm player wants to skip
+  var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
+  // if yes (true), leave fight
+  if (confirmSkip) {
+    window.alert(playerName + " has decided to skip this fight. Goodbye!");
+    // subtract money from playerMoney for skipping
+    playerMoney = playerMoney - 2;
+    
+  }
+  // if no (false), ask question again by running fight() again
+  else {
+    fight();
+  }
+  
+} else {
+    window.alert("You need to choose a valid option. Try again!");
+}
+  
 };
 
 //functions
