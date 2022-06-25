da9120dfe423c1c1d4ae366be5dba94c4411ba79
OmarD <omar.daghestani@colorado.edu>
2022-06-25 12:10:54 -0600

    worked on fight function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,13 +1,16 @@
+
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 
-// You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
 var enemyName = "Roborto";
 var enemyHealth = 50;
 var enemyAttack = 12;
+
+console.log(enemyName, enemyAttack, enemyHealth);
+
 
 var fight = function() {
   // Alert players that they are starting the round
@@ -22,7 +25,15 @@ var fight = function() {
   console.log(
     playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
   );
-  
+
+  // check enemy's health
+  if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died!");
+  } 
+  else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+  }
+
   // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
 
     playerHealth = playerHealth - enemyAttack;
@@ -33,4 +44,27 @@ var fight = function() {
     enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
   );
 
+  // check player's health
+  if (playerHealth <= 0) {
+    window.alert(playerName + " has died!");
+  } 
+  else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
+  }
+
+  // check to see if the value of the playerHealth variable is greater than 0
+  if (playerHealth > 0) {
+    console.log("Your player is still alive!");
+  }
+
+
 };
+
+//functions
+fight();
+
+
+
+
+
+
